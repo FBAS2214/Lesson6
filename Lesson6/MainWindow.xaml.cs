8f12ad7cc803430ff4d658d1b0c4adfff6dedffd
t.novruzov <t.novruzov@STHQ012E-01>
2022-06-24 21:14:28 +0400

    Add project files.

---
+++ Lesson6/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Lesson6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // Inline
+            // Internal
+            // External
+
+
+
+            // Property Trigger
+            // DataTrigger
+            // Event Trigger
+        }
+    }
+}
